657d52d5561551097f78cd711d9a6101813a852b
Akshata Jog <136689107+akshatasProjects@users.noreply.github.com>
2024-07-20 12:39:21 +0530

    Add files via upload

---
+++ JavaScriptProjects/30-JS-Projects/23. BMI-Calculator/script.js
@@ -0,0 +1,23 @@
+const btnEl = document.getElementById("btn");
+const bmiResultEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
+
+function calculateBMI() {
+  const heightValue = document.getElementById("height").value / 100;
+  const weightValue = document.getElementById("weight").value;
+
+  const bmiValue = weightValue / (heightValue * heightValue);
+  bmiResultEl.value = bmiValue;
+
+  if (bmiValue < 18.5) {
+    weightConditionEl.innerText = `"Under Weight"`;
+  } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+    weightConditionEl.innerText = `"Normal"`;
+  } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+    weightConditionEl.innerText = `"Over weight"`;
+  } else if (bmiValue >= 30) {
+    weightConditionEl.innerText = `"Obesity"`;
+  }
+}
+
+btnEl.addEventListener("click", calculateBMI);
