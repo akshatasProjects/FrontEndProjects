bb301be2c3ab98e44249e50c2c03b2f2b326c9ac
Akshata Jog <136689107+akshatasProjects@users.noreply.github.com>
2024-07-24 14:09:24 +0530

    Add files via upload

---
+++ JavaScriptProjects/30-JS-Projects/28.bgImageScroll/script.js
@@ -0,0 +1,11 @@
+const bgImage = document.getElementById("bg-image");
+
+window.addEventListener("scroll", () => {
+  updateImage();
+});
+
+function updateImage() {
+  bgImage.style.opacity = 1 - window.pageYOffset / 900;
+  bgImage.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+  // bgImage.style.backgroundSize = 160 - window.pageYOffset + "%";
+}
